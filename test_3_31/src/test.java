9a20ffe2067124a210af480b64d50f3cefd3d0cd
ghaoii <641517196>
2022-04-01 22:08:22 +0800

    代码提交

--- test_3_31/src/test.java
+++ test_3_31/src/test.java
@@ -1,6 +1,39 @@
+//public class test {
+//    public static void main(String[] args) {
+//        int a = 10;
+//        char c = 'w';
+//        Dog dog = new Dog();
+//        dog.name = "九九";
+//        dog.age = 1;
+//        dog.eat("狗粮");
+//    }
+//}
+//
+//class Dog{
+//    int age;
+//    String name;
+//    public void eat(String food)
+//    {
+//        System.out.println(name + "正在吃" + food);
+//    }
+//}
+
+//public class test{
+//    public static void main(String[] args) {
+//        System.out.println("hello world");
+//    }
+//}
+
 public class test {
     public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("my name is " + args[0]);
+        System.out.println("My first name is :" + args[0]);
+        System.out.println("My last name is :" + args[1]);
     }
 }
+
+
+
+
+
+
+
---
+++ test_4_1/src/test.java
@@ -0,0 +1,84 @@
+//public class test {
+//    public static void main(String[] args) {
+//        int a = 10;
+//        System.out.println(1.0/2);
+//    }
+//}
+
+//public class test {
+//    public static void main(String[] args) {
+//        System.out.println(1.1*1.1);
+//    }
+//}
+
+//public class test {
+//    public static void main(String[] args) {
+//        boolean a = true;
+//        boolean b = false;
+//        System.out.println(a == 1);
+//        System.out.println(b == 0);
+//    }
+//}
+
+//public class test {
+//    public static void main(String[] args) {
+//        String name = "我的名字是:\"昊天月\"";
+//        String test = "\t有制表符的字符串";
+//        String test3 = "\\你好\\";
+//        System.out.println(name);
+//        System.out.println(test);
+//        System.out.println(test3);
+//    }
+//}
+
+//public class test {
+//    public static void main(String[] args) {
+//        int a = 10;
+//        int b = 20;
+//        int c = a + b;
+//        //在Java中，任意数据类型只要和字符串"+"，都会把其他类型转斌为字符串然后进行字符串的拼接操作
+//        String ret = "计算结果:" + a + b;
+//        System.out.println(ret);
+//    }
+//}
+
+//public class test {
+//    public static void main(String[] args) {
+//        int a = 10;
+//        int b = 20;
+//        //一行输出打印多个变量的值，通常都是转为字符串后进行输出
+//        System.out.println("a = " + a + ",b = " + b);
+//    }
+//}
+
+//public class test {
+//    public static void main(String[] args) {
+//        int a = 10;
+//    }
+//    System.out.println(a);
+//}
+
+public class test {
+    public static void main(String[] args) {
+        int a = 10;
+        a = 20;
+        final int b = 30;
+        b = 40;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
