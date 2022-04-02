d5affe5493593b70a39ddba00d0183dc84fa28a8
ghaoii <641517196@qq.com>
2022-04-02 22:18:02 +0800

    代码提交

---
+++ test_4_2/src/Test.java
@@ -0,0 +1,137 @@
+//public class Test {
+//    public static void main(String[] args) {
+//        System.out.println("hello world");
+//    }
+//}
+
+//public class Test {
+//    public static void main(String[] args) {
+//        System.out.println(Integer.MAX_VALUE);
+//        System.out.println(Integer.MIN_VALUE);
+//    }
+//}
+
+//public class Test {
+//    public static void main(String[] args) {
+//        int a = Integer.MAX_VALUE;
+//        a = a + 1;
+//        System.out.println(a);
+//    }
+//}
+
+//public class Test {
+//    public static void main(String[] args) {
+//        long a = Long.MAX_VALUE;
+//        long b = 10l;
+//        System.out.println(a);
+//        System.out.println(b);
+//    }
+//}
+
+//public class Test {
+//    public static void main(String[] args) {
+//        System.out.println(1 / 2);
+//    }
+//}
+
+//public class Test {
+//    public static void main(String[] args) {
+//        System.out.println(1.0/2.0);
+//    }
+//}
+
+//public class Test {
+//    public static void main(String[] args) {
+//        double a = 1.1;
+//        double b = 1.1;
+//        System.out.println(a * b);
+//    }
+//}
+
+//public class Test {
+//    public static void main(String[] args) {
+//        double a = 1.2;
+//        double b = 1.2;
+//        System.out.println(a * b);
+//    }
+//}
+
+//public class Test {
+//    public static void main(String[] args) {
+//        boolean a = true;
+//        boolean b = false;
+//        System.out.println(a == 1);
+//        System.out.println(b == 0);
+//    }
+//}
+
+//public class Test {
+//    public static void main(String[] args) {
+//        String a = "你好";
+//        System.out.println(a);
+//    }
+//}
+
+//public class Test {
+//    public static void main(String[] args) {
+//        String name = "My name is : \"昊天月\"";
+//        String str = "\t有制表符的字符串";
+//        String test = "My name is : \\昊天月\\";
+//        System.out.println(name);
+//        System.out.println(str);
+//        System.out.println(test);
+//    }
+//}
+
+//public class Test {
+//    public static void main(String[] args) {
+//        int a = 10;
+//        int b = 20;
+//        String ret = "计算结果:" + (a + b);
+//        System.out.println(ret);
+//    }
+//}
+
+//public class Test {
+//    public static void main(String[] args) {
+//        int a = 10;
+//        int b = 20;
+//        //一行输出打印多个变量的值，通常都是转为字符串后进行输出
+//        System.out.println("a = " + a + ",b = " + b);
+//    }
+//}
+
+//public class Test {
+//    public static void main(String[] args) {
+//        int a = 10;
+//    }
+//    System.out.println(a);
+//}
+
+//public class Test {
+//    public static void main(String[] args) {
+//        int a = 10;
+//        a = 20;
+//        final int b = 30;
+//        b = 40;
+//        System.out.println(a);
+//        System.out.println(b);
+//    }
+//}
+
+//public class Test {
+//    public static void main(String[] args) {
+//        //直接写出来的就是字面量，字面量是常量
+//        System.out.println(10 + 'a' + 10.1 + "hello");
+//        System.out.println("hello " + 10 + 'a' + 10.1);
+//    }
+//}
+
+//public class Test {
+//    public static void main(String[] args) {
+//        long a = 100;
+//        long b = 100L;
+//        long c = 10000000000;
+//        long d = 10000000000L;
+//    }
+//}
